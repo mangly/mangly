d1fd74420dd1eaff27f2d7c3821dc6928e1f9a40
Héctor Armando Rizo Blanco <elmangly@gmail.com>
2018-12-01 22:47:13 -0500

    cambio en las funciones

--- controller.js
+++ controller.js
@@ -11,12 +11,7 @@ exports.Application = function()
     this.psmc_data_collection = [];
 }
 
-exports.Application.prototype.Add_Psmc_Data = function(graphic, theta, rho){
-    
-    this.psmc_data_collection.push(new Psmc_Data(graphic, theta, rho));
-}
-
-exports.Application.prototype.Plot = function(data, chart, path, python_script){
+exports.Application.prototype.Add_Psmc_Data = function(path, python_script){
 
     let options = {
         mode: 'text',
@@ -28,8 +23,11 @@ exports.Application.prototype.Plot = function(data, chart, path, python_script){
       
         else{
           var json = JSON.parse(results[0])
-          data.data.datasets.push(json)
-          chart.update() 
         }
     });
+}
+
+exports.Application.prototype.Plot = function(data, chart){
+    data.data.datasets.push(json)
+    chart.update() 
 }
