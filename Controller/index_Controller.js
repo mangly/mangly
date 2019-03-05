041f37f90b08882f09a801fcaead64119b1385da
Hector Armando Rizo Blanco <elmangly@gmail.com>
2019-03-05 15:20:09 -0500

    Disable multiple selection in checkbox control

--- Controller/index_Controller.js
+++ Controller/index_Controller.js
@@ -93,33 +93,12 @@ $(document).ready(function () {
 
         //Selections
         if ($(event.target).is('.custom-control-input')) {
+            // Disable multiple selection in checkbox control
+            $('.custom-control-input').not($(event.target)).prop('checked', false);
+            //------------
 
             selected_function = application.logic_application.Get_Function(($(event.target).parents('.custom-control').siblings('.listview__content').children('.listview__heading')).text());
             legend_color = $(event.target).parents('.custom-control').children('.custom-control--char__helper');
-
-            // Disable multiple selection in checkbox control
-            // $($(event.target)).click(function () {
-            //     $('.custom-control-input').not(this).prop('checked', false);
-            // });
-            //------------
-
-            $($(event.target)).on('click', function() {
-                console.log('ojk')
-                // in the handler, 'this' refers to the box clicked on
-                var $box = $(this);
-                if ($box.is(":checked")) {
-                  // the name of the box is retrieved using the .attr() method
-                  // as it is assumed and expected to be immutable
-                  var group = "input:checkbox[name='" + $box.attr("name") + "']";
-                  // the checked state of the group/box on the other hand will change
-                  // and the current value is retrieved using .prop() method
-                  $(group).prop("checked", false);
-                  $box.prop("checked", true);
-                } else {
-                  $box.prop("checked", false);
-                }
-              });
-
 
             if ($(event.target).prop('checked')) {
                 $('#reset-scales').removeAttr('disabled');
@@ -160,7 +139,7 @@ $(document).ready(function () {
                 selected_function = null;
                 // slider_s.noUiSlider.set(100);
                 slider_mu.noUiSlider.set(1.25);
-                
+
                 application.Initialize_Information_Of_Functions();
                 // legend_color = [];
                 // items_selecteds = [];
